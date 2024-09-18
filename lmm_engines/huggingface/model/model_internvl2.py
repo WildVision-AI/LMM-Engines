ec81dc2a2c6adbc7666a21b49c21c18b772f9c8c
chchenhui <chenhui19960614@163.com>
2024-09-18 08:36:49 +0000

    add split_model to serve InternVL2-Llama3-76B on multiple GPUs

--- lmm_engines/huggingface/model/model_internvl2.py
+++ lmm_engines/huggingface/model/model_internvl2.py
@@ -2,6 +2,7 @@ import torch
 from PIL import Image
 import json
 import time
+import math
 import numpy as np
 from io import BytesIO
 from .model_adapter import BaseModelAdapter, register_model_adapter
@@ -9,6 +10,7 @@ from ..conversation import get_conv_template, Conversation
 from .videollm_utils.internvl2.utils import load_video
 from ...utils import decode_and_save_video
 from transformers import TextIteratorStreamer, AutoModel, AutoProcessor, AutoTokenizer
+from transformers.utils import is_flash_attn_2_available
 from threading import Thread
 from typing import List
 
@@ -21,6 +23,33 @@ InternVL2_MODEL_LIST = (
     "InternVL2‑40B",
     "InternVL2-Llama3-76B",
 )
+
+def split_model(model_name):
+    device_map = {}
+    world_size = torch.cuda.device_count()
+    num_layers = {
+        'InternVL2-1B': 24, 'InternVL2-2B': 24, 'InternVL2-4B': 32, 'InternVL2-8B': 32,
+        'InternVL2-26B': 48, 'InternVL2-40B': 60, 'InternVL2-Llama3-76B': 80}[model_name]
+    # Since the first GPU will be used for ViT, treat it as half a GPU.
+    num_layers_per_gpu = math.ceil(num_layers / (world_size - 0.5))
+    num_layers_per_gpu = [num_layers_per_gpu] * world_size
+    num_layers_per_gpu[0] = math.ceil(num_layers_per_gpu[0] * 0.5)
+    layer_cnt = 0
+    for i, num_layer in enumerate(num_layers_per_gpu):
+        for j in range(num_layer):
+            device_map[f'language_model.model.layers.{layer_cnt}'] = i
+            layer_cnt += 1
+    device_map['vision_model'] = 0
+    device_map['mlp1'] = 0
+    device_map['language_model.model.tok_embeddings'] = 0
+    device_map['language_model.model.embed_tokens'] = 0
+    device_map['language_model.output'] = 0
+    device_map['language_model.model.norm'] = 0
+    device_map['language_model.lm_head'] = 0
+    device_map['language_model.model.rotary_emb'] = 0
+    device_map[f'language_model.model.layers.{num_layers - 1}'] = 0
+
+    return device_map
 
 class InternVL2Adapter(BaseModelAdapter):
     """The model adapter for InternVL2"""
@@ -42,6 +71,12 @@ class InternVL2Adapter(BaseModelAdapter):
         from_pretrained_kwargs["torch_dtype"] = torch.bfloat16
         from_pretrained_kwargs["low_cpu_mem_usage"] = True
         from_pretrained_kwargs["trust_remote_code"] = True
+        if is_flash_attn_2_available():
+            from_pretrained_kwargs["use_flash_attn"] = True
+        if torch.cuda.device_count() > 1:
+            model_name = model_path.split("/")[1]
+            device_map = split_model(model_name)
+            from_pretrained_kwargs["device_map"] = device_map
         self.model = AutoModel.from_pretrained(
             model_path, **from_pretrained_kwargs
         ).eval()
